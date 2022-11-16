ad89d7aebc09480b0f338ac3831bfa9dec6f57e1
Vipin Soni <sonivipin5@gmail.com>
2022-11-16 10:40:11 +0530

    mybblog project code

---
+++ backendcode/config/api.js
@@ -0,0 +1,11 @@
+  // path: ./config/api.js
+  module.exports = ({ env }) => ({
+    responses: {
+      privateAttributes: ['_v', 'id', 'created_at'],
+    },
+    rest: {
+      prefix: '/v1',
+      defaultLimit: 100,
+      maxLimit: 250,
+    },
+  });
